eca9095ee8f5097b7889d8e0d85a69ab7b5b5690
alvarojguerrero <guerreroalvarojavier@gmail.com>
2021-11-08 15:54:34 -0500

    Update Grupo.java

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -17,8 +17,8 @@ public class Grupo {
     }
 
     Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        Persona[] personas = new Persona[cantidadEstudiantes];
         this(profesor, asignatura, codigo, horario);
+        Persona[] personas = new Persona[cantidadEstudiantes];
         this.setEstudiantes(personas);
     }
 
